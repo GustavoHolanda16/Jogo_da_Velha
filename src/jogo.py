a85e8b3b87e50e939441e616be8b36b97f57557a
Gustavo Holanda de Sousa <106280222+GustavoHolanda16@users.noreply.github.com>
2024-01-17 22:55:32 -0300

    fim

--- src/jogo.py
+++ src/jogo.py
@@ -1,15 +1,52 @@
 import random
 import os
 
+tipo = ''
+cont = True
 tabu = [' ' for _ in range(9)]
 ranking = []
 
-class MeuObjeto:
-    def __init__(self, nome, vitorias, derrotas, empates):
+def tabela_ranking():
+    limpar_tela()
+    ranking_ordenado = sorted(ranking, key=lambda x: x.vitorias, reverse=True)
+    print("\n{:<15} {:<10} {:<10} {:<10}".format("Nome", "Vitórias", "Derrotas", "Empates"))
+    print("-" * 45)
+    for jogador in ranking_ordenado:
+        print("{:<15} {:<10} {:<10} {:<10}".format(
+            jogador.nome, jogador.vitorias, jogador.derrotas, jogador.empates
+        ))
+
+class Jogador:
+    def __init__(self, nome):
+        self.tipo = tipo
         self.nome = nome
-        self.vitorias = vitorias
-        self.derrotas = derrotas
-        self.empates = empates
+        self.vitorias = 0
+        self.derrotas = 0
+        self.empates = 0
+
+    def vitoria(self):
+        self.vitorias += 1
+
+    def derrota(self):
+        self.derrotas += 1
+
+    def empate(self):
+        self.empates += 1
+
+def verificar_jogador(nome):
+    for jogador in ranking:
+        if jogador.nome == nome:
+            return jogador
+
+    novo_jogador = Jogador(nome)
+    if tipo == "X":
+        novo_jogador.tipo = "X"
+    elif tipo == "O":
+        novo_jogador.tipo = "O"
+
+    ranking.append(novo_jogador)
+    return novo_jogador
+
 def tabuleiro():
     print(f" {tabu[0]} | {tabu[1]} | {tabu[2]} ")
     print("---+---+---")
@@ -26,11 +63,20 @@ def vazio():
     print('O jogo empatou!')
     return False
 
-def test_fazer_jogada(jogador):
+def test_fazer_jogadaX(jogador):
     while True:
-        move = int(input(f"Vez do jogador {jogador}, escolha uma posição (0-8): "))
+        move = int(input(f"Vez do jogador {jogador.nome}, escolha uma posição (0-8): "))
         if 0 <= move <= 8 and tabu[move] == ' ':
-            tabu[move] = jogador
+            tabu[move] = 'X'
+            break
+        else:
+            print("Posição inválida ou já preenchida. Tente novamente.")
+
+def test_fazer_jogadaO(jogador):
+    while True:
+        move = int(input(f"Vez do jogador {jogador.nome}, escolha uma posição (0-8): "))
+        if 0 <= move <= 8 and tabu[move] == ' ':
+            tabu[move] = 'O'
             break
         else:
             print("Posição inválida ou já preenchida. Tente novamente.")
@@ -67,47 +113,75 @@ def check_empate():
     return ' ' not in tabu
 
 def game():
+    global tabu
+    tabu = [' ' for _ in range(9)]
+    nome1 = input('Digite o nome do jogador X:')
+    jogador1 = verificar_jogador(nome1)
+    nome2 = input('Digite o nome do jogador O:')
+    jogador2 = verificar_jogador(nome2)
+
     while True:
         limpar_tela()
         tabuleiro()
-        test_fazer_jogada('X')
+        test_fazer_jogadaX(jogador1)
         if verificar_vitoria('X'):
             tabuleiro()
-            print("Jogador X venceu!")
-            return
+            jogador1.vitoria()
+            jogador2.derrota()
+            print(f"Jogador {jogador1.nome} venceu!")
+            break
         if not vazio():
-            return
+            jogador1.empate()
+            jogador2.empate()
+            break
         limpar_tela()
         tabuleiro()
-        test_fazer_jogada('O')
+        test_fazer_jogadaO(jogador2)
         if verificar_vitoria('O'):
             tabuleiro()
-            print("Jogador O venceu!")
-            return
+            jogador2.vitoria()
+            jogador1.derrota()
+            print(f"Jogador {jogador2.nome} venceu!")
+            break
         if not vazio():
-            return
+            jogador1.empate()
+            jogador2.empate()
+            break
 
 def gameCom():
+    global tabu
+    tabu = [' ' for _ in range(9)]
+    nome1 = input('Digite o nome do jogador X:')
+    jogador1 = verificar_jogador(nome1)
+    nome2 = 'PC'
+    jogador2 = verificar_jogador(nome2)
     while True:
         limpar_tela()
         tabuleiro()
-        test_fazer_jogada('X')
+        test_fazer_jogadaX(jogador1)
         if verificar_vitoria('X'):
             tabuleiro()
-            print("Jogador X venceu!")
-            return
+            jogador1.vitoria()
+            jogador2.derrota()
+            print(f"Jogador {jogador1.nome} venceu!")
+            break
         if not vazio():
-            return
+            jogador1.empate()
+            jogador2.empate()
+            break
         limpar_tela()
         tabuleiro()
         jogada_computador()
         if verificar_vitoria('O'):
-            limpar_tela()
             tabuleiro()
-            print("Jogador O (computador) venceu!")
-            return
+            jogador2.vitoria()
+            jogador1.derrota()
+            print(f"Jogador {jogador2.nome} venceu!")
+            break
         if not vazio():
-            return
+            jogador1.empate()
+            jogador2.empate()
+            break
 
 def menu_principal():
     print('Escolha uma das opções...')
@@ -115,19 +189,19 @@ def menu_principal():
     print('2 -- Para Jogar contra o PC')
     print('3 -- Para ver Ranking')
     print('4 -- Para sair')
-    x = input(': ')
+    x = input('Digite sua opção: ')
 
     if x == '1':
+        limpar_tela()
         game()
     elif x == '2':
+        limpar_tela()
         gameCom()
+    elif x == '3':
+        limpar_tela()
+        tabela_ranking()
+    elif x == '4':
+        exit()
 
-menu_principal()
-
-
-
-
-
-
-
-
+while cont:
+    menu_principal()
